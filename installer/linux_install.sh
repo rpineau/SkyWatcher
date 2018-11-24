fc388a35376bcf4244bdc73d31b767627b5be07b
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:44:51 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -18,20 +16,27 @@ fi
 
 echo "Installing to $TheSkyX_Path"
 
+
 if [ ! -d "$TheSkyX_Path" ]; then
     echo TheSkyX Install dir not exist
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./mountlist Skywatcher.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./Skywatcher.ui" "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/"
-cp "./libSkywatcher.so" "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/"
+cp "./Skywatcher.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/"
+cp "./libSkywatcher.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/mountlist Skywatcher.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/Skywatcher.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/libSkywatcher.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/Skywatcher.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/libSkywatcher.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/libSkywatcher.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/libSkywatcher.so"
 
