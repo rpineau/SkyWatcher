782903175dfbe4b6ac3ee0fdae69de3d99bffefd
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 19:02:50 -0800

    Fix macOS 64 bit code signing

--- installer/build_notarize.sh
+++ installer/build_notarize.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+PACKAGE_NAME="Skywatcher_X2.pkg"
+BUNDLE_NAME="org.rti-zone.SkywatcherX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libSkywatcher.dylib
+fi
+
 mkdir -p ROOT/tmp/Skywatcher_X2/
 cp "../Skywatcher.ui" ROOT/tmp/Skywatcher_X2/
 cp "../mountlist Skywatcher.txt" ROOT/tmp/Skywatcher_X2/
 cp "../build/Release/libSkywatcher.dylib" ROOT/tmp/Skywatcher_X2/
 
 
-PACKAGE_NAME="Skywatcher_X2.pkg"
-BUNDLE_NAME="org.rti-zone.SkywatcherX2"
 
 if [ ! -z "$installer_signature" ]; then
 	# signed package using env variable installer_signature
